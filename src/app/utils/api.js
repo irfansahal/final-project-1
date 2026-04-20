392f5264285868ed5df19bc71728be383bfa1b3f
irfan sahal <irfansahal1998@gmail.com>
2026-04-20 18:27:53 +0600

    update

--- src/app/utils/api.js
+++ src/app/utils/api.js
@@ -2,8 +2,8 @@ export const getBaseUrl = () => {
     if(process.env.VERCEL_PROJECT_PRODUCTION_URL){
         return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
     }
-    if(process.env.NEXTAUTH_URL){
-       return process.env.NEXTAUTH_URL;
-    }
+    // if(process.env.NEXTAUTH_URL){
+    //    return process.env.NEXTAUTH_URL;
+    // }
     return `http://localhost:3000`;
 }
